ea5ac1c8fd5a0de4909bd4dca1d598816f10f76e
Aviwe Ngqukumba <aviwe3226@gmail.com>
2022-07-29 13:08:55 +0200

    Added number of followers, following, repos and gists to the user view. Also added their corresponding routes and pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UsersView from '../views/UsersView.vue'
 import UserView from '../views/UserView.vue'
+import FollowersView from '../views/FollowersView.vue'
+import FollowingView from '../views/FollowingView.vue'
+import ReposView from '../views/ReposView.vue'
+import GistsView from '../views/GistsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +21,30 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: UserView,
+      props: true,
+    },
+    {
+      path: '/:username/followers',
+      name: 'followers',
+      component:FollowersView,
+      props: true,
+    },
+    {
+      path: '/:username/following',
+      name: 'following',
+      component: FollowingView,
+      props: true,
+    },
+    {
+      path: '/:username/repos',
+      name: 'repos',
+      component: ReposView,
+      props: true,
+    },
+    {
+      path: '/:username/gists',
+      name: 'gists',
+      component: GistsView,
       props: true,
     }
   ]
--- src/views/FollowingView.vue
+++ src/views/FollowingView.vue
@@ -2,10 +2,10 @@
 import axios from "axios";
 
 export default {
-  name: "User",
+  name: "Following",
   data() {
     return {
-      user: null,
+      following: null,
     };
   },
     props:{
@@ -15,9 +15,9 @@ export default {
       },
     },
     created: function () {
-    axios.get(`https://api.github.com/users/${this.username}`).then((response) => {
-      this.user = response.data;
-      console.log(this.user);
+    axios.get(`https://api.github.com/users/${this.username}/following`).then((response) => {
+      this.following = response.data;
+      console.log(this.following);
     });
   },
 }
@@ -28,14 +28,14 @@ export default {
     <section class="">
       <div class="">
         <div class="container">
-          <h1 class="title">Get GitHub User</h1>
+          <h1 class="title">Get GitHub User Following</h1>
           <h2 class="subtitle">using Vue JS, REST API</h2>
         </div>
       </div>
     </section>
     <div class="container">
       <div >
-        <table >
+        <!-- <table >
           <thead>
             <tr>
               <th>Profile Image</th>
@@ -43,9 +43,6 @@ export default {
               <th>Fullname</th>
               <th>Bio</th>
               <th>Number of followers</th>
-              <th>Number of following</th>
-              <th>Number of repos</th>
-              <th>Number of gists</th>
             </tr>
           </thead>
           <tbody>
@@ -54,13 +51,10 @@ export default {
               <td>{{ user?.login }}</td>
               <td>{{ user?.name ? user?.name: "No Name"}}</td>
               <td>{{ user?.bio }}</td>
-              <td><RouterLink :to="{ name: 'followers', params: {username} }">{{user?.followers}}</RouterLink></td>
-              <td><RouterLink :to="{ name: 'following', params: {username} }">{{user?.following}}</RouterLink></td>
-              <td><RouterLink :to="{ name: 'repos', params: {username} }">{{user?.public_repos}}</RouterLink></td>
-              <td><RouterLink :to="{ name: 'gists', params: {username} }">{{user?.public_gists}}</RouterLink></td>
+              <td><RouterLink :to="{ name: 'followers', params: {username} }">Number of followers</RouterLink></td>
             </tr>
           </tbody>
-        </table>
+        </table> -->
       </div>
     </div>
   </div>
